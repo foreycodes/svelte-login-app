046438ed7b0489a3e26398a1bc08b0072689b7f3
eforrester <esfoures@gmail.com>
2023-01-14 13:46:31 -0500

    Load API from Spoonacular

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from "svelte"; 
+
+    let data; 
+    // fetching data from spoonacular
+    onMount(async ()=> {
+        const res = await fetch("https://api.spoonacular.com/recipes/complexSearch?&apiKey=9f5a00eb2a454d6893455a09e3ef7964")
+
+        const data = await res.json()
+        console.log(data)
+    })
+</script>
+<h1>Home</h1>
